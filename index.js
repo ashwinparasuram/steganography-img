00ba1528767be1b2551aa0d76b1b7136db5cc225
Ashwin Parasuram <62600349+ashwinparasuram@users.noreply.github.com>
2021-12-13 21:52:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,127 @@
+var imgdatauri;
+var encodedurl;
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      document.querySelector("#image1").src = e.target.result;
+      imgdatauri = e.target.result;
+    };
+  }
+  reader.readAsDataURL(input.files[0]);
+}
+
+
+function decode(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+  
+      reader.onload = function(e) {
+          console.log(steg.decode(e.target.result));
+          data = steg.decode(e.target.result);
+          console.log(data);
+        
+        document.querySelector('#decoded').innerText = decrypt(data);
+      };
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+
+
+function hideText() {  
+  document.querySelector("#image2").src = steg.encode(encrypt(document.querySelector('#text').value), imgdatauri);
+  encodedurl = document.querySelector("#image2").src;
+}
+
+function DownloadImage() {    
+  document.getElementById("imageurl").href = encodedurl;     
+}
+
+const encrypt = (message)=>{
+  let encryptedMessage;
+
+  const messageArr = message.split("");
+  //console.log('messafe array', messageArr);
+
+let encryptedMessageArr = [];
+
+for(i=0; i<messageArr.length; i++){
+  let letter;
+  switch(messageArr[i]){
+    case "a":
+      letter = "!";
+      break;
+
+      case "e":
+      letter = "@";
+      break;
+
+      case "i":
+      letter = "#";
+      break;
+
+      case "o":
+      letter = "$";
+      break;
+
+      case "u":
+      letter = "%";
+      break;
+
+      default:
+      letter = messageArr[i];
+      break;   
+  }
+encryptedMessageArr.push(letter);
+//console.log("encrypted message array", encryptedMessageArr)
+
+}
+encryptedMessage = encryptedMessageArr.join("");
+return encryptedMessage;
+};
+
+const decrypt = (message)=>{
+  let decryptedMessage;
+
+  const messageArr = message.split("");
+  //console.log('messafe array', messageArr);
+
+let decryptedMessageArr = [];
+
+for(i=0; i<messageArr.length; i++){
+  let letter;
+  switch(messageArr[i]){
+    case "!":
+      letter = "a";
+      break;
+
+      case "@":
+      letter = "e";
+      break;
+
+      case "#":
+      letter = "i";
+      break;
+
+      case "$":
+      letter = "o";
+      break;
+
+      case "%":
+      letter = "u";
+      break;
+
+      default:
+      letter = messageArr[i];
+      break;   
+  }
+decryptedMessageArr.push(letter);
+//console.log("encrypted message array", encryptedMessageArr)
+
+}
+decryptedMessage = decryptedMessageArr.join("");
+return decryptedMessage;
+};
+
